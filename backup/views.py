4a1717fa6784ccefbcd52247193461dadf9095fa
Suliman Awad <sulimanawadstudy@gmail.com>
2024-03-21 17:50:39 +0300

    fix backup error

--- backup/views.py
+++ backup/views.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.conf import settings
 from django.core.management import call_command
-from rest_framework.response import Response
+from django.http.response import HttpResponse
 from rest_framework.generics import RetrieveAPIView
 from common.permissions import IsSuperuser
 
@@ -11,7 +11,7 @@ class Backup(RetrieveAPIView):
     
     def retrieve(self, request, *args, **kwargs):
         now = str(timezone.now()).replace(' ', 'T').replace(':', '-')[:19]
-        filename = f"{settings.BACKUP_FILE_PREFIX}-{now}.dump"
+        filename = f"{settings.BACKUP_FILE_PREFIX}-{now}.sql"
         backup_file_path = f'{settings.DBBACKUP_STORAGE_OPTIONS["location"]}{filename}'
 
         call_command('dbbackup', output_filename=filename)
@@ -19,7 +19,7 @@ class Backup(RetrieveAPIView):
         with open(backup_file_path, 'rb') as backup_file:
             backup_content = backup_file.read()
 
-        response = Response(backup_content, content_type='application/octet-stream')
+        response = HttpResponse(backup_content, content_type='application/sql')
         response['Content-Disposition'] = f'attachment; filename="{filename}"'
 
         return response
