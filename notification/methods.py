9121df9a1c27a805a89e459904fddcc6533f8aa7
Suliman Awad <sulimanawadstudy@gmail.com>
2024-03-23 18:36:36 +0300

    upgrade notification api to let signle language sending

--- notification/methods.py
+++ notification/methods.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from firebase_admin import messaging
 from fcm_django.models import FCMDevice
-from translation.cache import get_default_language_code
 from translation.fields import UpdateTranslationField
 from notification.models import Notification
 
@@ -57,38 +56,41 @@ def save_notification(
     
 
 class ValidateTranslatedNotificationSerializer(serializers.Serializer):
-    title = UpdateTranslationField()
-    body = UpdateTranslationField()
+    title = UpdateTranslationField(base_is_enough=True)
+    body = UpdateTranslationField(base_is_enough=True)
     image = serializers.CharField()
 
 
 def push_notifications(
         users: list,
-        title: dict,
-        body: dict,
+        title: dict | str,
+        body: dict | str,
         image: str,
         save: bool = True,
         **data
     ):
-    ValidateTranslatedNotificationSerializer(data={
+    serializer = ValidateTranslatedNotificationSerializer(data={
         'title': title,
         'body': body,
         'image': image,
-    }).is_valid(raise_exception=True)
-
-    default_language_code = get_default_language_code()
-    default_title = title.get(default_language_code, '')
-    default_body = body.get(default_language_code, '')
+    })
+    serializer.is_valid(raise_exception=True)
+    validated_data = serializer.validated_data
+    title = validated_data['title']
+    body = validated_data['body']
+    image = validated_data['image']
 
     language_code_map = {}
     for user in users:
         language_code_map.setdefault(user.language_code, []).append(user)
 
+    title_base = title.get('base')
+    body_base = body.get('base')
     for language_code, temp_users in language_code_map.items():
         _push_notifications(
             temp_users,
-            title=title.get(language_code, default_title),
-            body=body.get(language_code, default_body),
+            title=title.get(language_code, title_base),
+            body=body.get(language_code, body_base),
             image=image,
             **data
         )
