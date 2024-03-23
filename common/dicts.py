2b53cb832642b6041db5ecdc9668ba09f3b5906c
Suliman Awad <sulimanawadstudy@gmail.com>
2024-03-23 17:45:59 +0300

    create DefaultDict class

---
+++ common/dicts.py
@@ -0,0 +1,20 @@
+class DefaultDict(dict):
+    def __init__(self, default=None, initial_dict={}):
+        super().__init__()
+        self.update(initial_dict)
+        self.default = default
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return self.default
+    
+    def get(self, key, default=None):
+        if default is None:
+            default = self.default
+        return super().get(key, default)
+
+
+
+
