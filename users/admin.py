d671065cef8c5113d4d8559659573450331590ea
Suliman Awad <sulimanawadstudy@gmail.com>
2024-03-26 12:23:27 +0300

    .

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from users.models import User
 from users.forms import CustomAuthenticationForm
 
 
 admin.site.login_form = CustomAuthenticationForm
-# admin.site.unregister(Group)
 
 
 @admin.register(User)
