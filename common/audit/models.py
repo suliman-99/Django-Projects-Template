e134413a4d339f6601d2179061c19b87591121f3
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-03 13:08:35 +0300

    upgrade the audit models

--- common/audit/models.py
+++ common/audit/models.py
@@ -5,13 +5,25 @@ from simple_history.models import HistoricalRecords
 
 
 class AuditModel(SafeDeleteModel):
+    '''
+    Use `AuditModel` when you want to register HistoricalRecords after doing something with the concrete model
+    Main example: Using django-modeltranslation in the model then using the django history 
+    '''
     class Meta:
         abstract = True
 
     _safedelete_policy = SOFT_DELETE_CASCADE
 
-    history = HistoricalRecords(inherit=True)
-
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
     updated_at = models.DateTimeField(null=True, blank=True, auto_now=True)
+
+
+class HistoricalAuditModel(AuditModel):
+    '''
+    Use `HistoricalAuditModel` when you want to register HistoricalRecords automatically right after initializing the class
+    '''
+    class Meta:
+        abstract = True
+
+    history = HistoricalRecords(inherit=True)
