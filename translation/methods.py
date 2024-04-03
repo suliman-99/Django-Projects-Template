32992430c391ca949ad487eaad8458e5164400db
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-03 17:42:12 +0300

    (use django-modeltranslation package) - last upgrade to the translation basic logic.

--- translation/methods.py
+++ translation/methods.py
@@ -14,3 +14,18 @@ def get_languages_codes():
 
 def get_default_language_code():
     return settings.LANGUAGES[0][0]
+
+
+def translate(name, value=None) -> list[str] | dict[str, any]:
+    '''
+    if value is None return ['name_en', 'name_ar',  ... ]
+    else return {'name_en': value, 'name_ar': value,  ... }
+    '''
+    ret = [ f'{name}_{code}' for code in get_languages_codes() ]
+    if value:
+        ret = { name: value for name in ret }
+    return ret
+
+
+def translation_field_required_kwargs(name):
+    return translate(name, { 'required': True, 'allow_null': False, 'allow_blank': False })
