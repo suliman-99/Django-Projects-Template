7d59b3a38b784f8e10daa0e9e3ab85a10566fce0
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-04 11:32:20 +0300

    upgrade the notification List APIs filters

--- notification/filters.py
+++ notification/filters.py
@@ -1,4 +1,6 @@
 from django_filters import rest_framework as filters
+from common.filters import FilterLookupExpr
+from translation.methods import translate
 from notification.models import Notification
 
 
@@ -6,6 +8,8 @@ class NotificationFilter(filters.FilterSet):
     class Meta:
         model = Notification
         fields = {
-            'user': ['exact'],
-            'is_viewed': ['exact'],
+            'user': FilterLookupExpr.OTHER,
+            'is_viewed': FilterLookupExpr.BOOLEAN,
+            **translate('title', FilterLookupExpr.STRING),
+            **translate('body', FilterLookupExpr.STRING),
         }
