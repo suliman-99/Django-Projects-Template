03280472cff5ad4cfbabc15381839f1695ce1cac
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-06 00:36:51 +0300

    upgrade FilterSet classes

--- logger/filters.py
+++ logger/filters.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+from common.filters import FilterLookupExpr
 from logger.models import Log
 
 
@@ -6,11 +7,11 @@ class LogFilter(filters.FilterSet):
     class Meta:
         model = Log
         fields = {
-            'created_at': ['lt', 'lte', 'gt', 'gte'],
-            'level': ['exact'],
-            'type': ['exact'],
-            'user': ['exact'],
-            'method': ['exact'],
-            'url': ['exact', 'contains', 'icontains'],
-            'message': ['exact'],
+            'created_at': FilterLookupExpr.DATETIME,
+            'level': FilterLookupExpr.STRING,
+            'type': FilterLookupExpr.STRING,
+            'user': FilterLookupExpr.OTHER,
+            'method': FilterLookupExpr.STRING,
+            'url': FilterLookupExpr.STRING,
+            'message': FilterLookupExpr.STRING,
         }
--- users/filters.py
+++ users/filters.py
@@ -1,12 +1,13 @@
 from django_filters import rest_framework as filters
 from django.contrib.auth.models import Permission
+from common.filters import FilterLookupExpr
 
 
 class PermissionFilter(filters.FilterSet):
     class Meta:
         model = Permission
         fields = {
-            'content_type': ['exact'],
-            'content_type__app_label': ['exact'],
-            'content_type__model': ['exact'],
+            'content_type': FilterLookupExpr.OTHER,
+            'content_type__app_label': FilterLookupExpr.STRING,
+            'content_type__model': FilterLookupExpr.STRING,
         }
