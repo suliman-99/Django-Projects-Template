db4258776b4416ac2881bde96c555aa06b165780
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-18 10:43:15 +0300

    chande data to results in response

--- common/middlewares.py
+++ common/middlewares.py
@@ -9,10 +9,10 @@ class ResponseCoordinatorMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        response:Response = self.get_response(request)
+        response: Response = self.get_response(request)
         if not request.path.startswith('/api/') or not response.headers.get('Content-Type') == 'application/json':
             return response
-        
+
         response_data = json.loads(response.content)
 
         if not TEMPLATE_FLAG in response_data:
@@ -20,18 +20,18 @@ class ResponseCoordinatorMiddleware:
                 if isinstance(response_data, dict) and response_data.pop(PAGINATION_FLAG, None):
                     new_response_data = success_response({})
                     new_response_data.update(response_data)
-                    
-                    # This line just to make the data as lower as possible in the response
+
+                    # This line just to make the results as lower as possible in the response
                     # Deleting This line will not effect any thing
-                    new_response_data['data'] = new_response_data.pop('data') 
-                    
+                    new_response_data['results'] = new_response_data.pop('results')
+
                     response_data = new_response_data
                 else:
                     response_data = success_response(response_data)
 
             else:
                 response_data = fail_response(
-                    message=response_data['detail'], 
+                    message=response_data['detail'],
                     error_field=response_data.get('error_field'),
                 )
 
