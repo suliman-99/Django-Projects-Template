0f13560f5811b75a2784b38992d0ce24f257326b
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-21 05:40:24 +0300

    coordinating the models fields

---
+++ common/models.py
@@ -0,0 +1,16 @@
+from typing import Any
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+
+class SVGField(models.FileField):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        kwargs.setdefault('validators', []).append(FileExtensionValidator(['svg']))
+        super().__init__(*args, **kwargs)
+
+
+class CustomDecimalField(models.DecimalField):
+    def __init__(self, *args, **kwargs) -> None:
+        kwargs['max_digits'] = 20
+        kwargs['decimal_places'] = 6
+        super().__init__(*args, **kwargs)
