41974941f1bbcb23a81ab05c976486cd03118bef
Suliman Awad <sulimanawadstudy@gmail.com>
2024-04-21 05:48:56 +0300

    upgrade model fields

--- common/models.py
+++ common/models.py
@@ -3,13 +3,24 @@ from django.core.validators import FileExtensionValidator
 
 
 class SVGField(models.FileField):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, **kwargs):
         kwargs.setdefault('validators', []).append(FileExtensionValidator(['svg']))
         super().__init__(*args, **kwargs)
 
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs["validators"]
+        return name, path, args, kwargs
+
 
 class CustomDecimalField(models.DecimalField):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, **kwargs):
         kwargs['max_digits'] = 20
         kwargs['decimal_places'] = 6
         super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs["max_digits"]
+        del kwargs["decimal_places"]
+        return name, path, args, kwargs
