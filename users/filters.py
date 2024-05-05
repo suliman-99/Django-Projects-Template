6c0d9625fca79fe2d92e4f176d66e8dcc130d485
Suliman Awad <sulimanawadstudy@gmail.com>
2024-05-05 19:01:42 +0300

    make UserFilter class for the user crud API

--- users/filters.py
+++ users/filters.py
@@ -1,13 +1,39 @@
-from django_filters import rest_framework as filters
 from django.contrib.auth.models import Permission
+from django_filters import rest_framework as filters
 from common.rest_framework.filters import FilterLookupExpr
+from users.models import User
 
 
 class PermissionFilter(filters.FilterSet):
     class Meta:
         model = Permission
         fields = {
+            'name': FilterLookupExpr.STRING,
             'content_type': FilterLookupExpr.OTHER,
-            'content_type__app_label': FilterLookupExpr.STRING,
-            'content_type__model': FilterLookupExpr.STRING,
+            'content_type__app_label': FilterLookupExpr.OTHER,
+            'content_type__model': FilterLookupExpr.OTHER,
+        }
+
+
+class UserFilter(filters.FilterSet):
+    class Meta:
+        model = User
+        fields = {
+            'is_active': FilterLookupExpr.BOOLEAN,
+            'is_admin': FilterLookupExpr.BOOLEAN,
+
+            'date_joined': FilterLookupExpr.DATE,
+            'first_login': FilterLookupExpr.DATETIME,
+            'last_login': FilterLookupExpr.DATETIME,
+            'last_refresh': FilterLookupExpr.DATETIME,
+
+            'email': FilterLookupExpr.STRING,
+            'email_verified': FilterLookupExpr.BOOLEAN,
+
+            'phone_number': FilterLookupExpr.STRING,
+            'phone_number_verified': FilterLookupExpr.BOOLEAN,
+
+            'first_name': FilterLookupExpr.STRING,
+            'last_name': FilterLookupExpr.STRING,
+            'language_code': FilterLookupExpr.OTHER,
         }
