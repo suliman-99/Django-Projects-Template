3367a066ed43a5b226793bddc510a6d03174caf5
Suliman Awad <sulimanawadstudy@gmail.com>
2024-09-22 14:29:54 +0300

    upgrade serializers.

--- common/rest_framework/serializers.py
+++ common/rest_framework/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 
 
-class CustomImageField(serializers.ImageField):
+class CustomFileField(serializers.ImageField):
     def to_representation(self, value):
-        if value.name.startswith('http'):
+        if value and value.name.startswith('http'):
             return value.name
         return super().to_representation(value)
