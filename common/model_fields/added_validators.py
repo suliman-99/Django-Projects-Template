538bf9ae7d5cdbe9317ac3a495c0aa43124e5905
Suliman Awad <sulimanawadstudy@gmail.com>
2024-09-22 15:17:28 +0300

    fix mistakes in model_fields.

--- common/model_fields/added_validators.py
+++ common/model_fields/added_validators.py
@@ -12,7 +12,7 @@ class AddedValidatorsPlug:
         name, path, args, kwargs = super().deconstruct()
         added_validators = self.get_added_validators()
         validators = kwargs.get('validators', [])
-        validators = filter(lambda o: o not in added_validators, validators)
+        validators = list(filter(lambda o: o not in added_validators, validators))
         kwargs['validators'] = validators
         return name, path, args, kwargs
     
--- common/model_fields/svg.py
+++ common/model_fields/svg.py
@@ -3,5 +3,5 @@ from .base_file import CustomFileField
 from .added_validators import AddedValidatorsPlug
 
 
-class PNGField(AddedValidatorsPlug, CustomFileField):
+class SVGField(AddedValidatorsPlug, CustomFileField):
     added_validators = (validators.FileExtensionValidator(['svg']), )
