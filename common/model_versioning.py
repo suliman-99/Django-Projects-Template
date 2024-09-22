510d743291e2491123f429b919f06fe181102127
Suliman Awad <sulimanawadstudy@gmail.com>
2024-09-22 14:19:03 +0300

    fix a critical error in model_versioning.

--- common/model_versioning.py
+++ common/model_versioning.py
@@ -7,11 +7,16 @@ def model_versioning_signal_handler(origin_model, version_model):
     @receiver(post_save, sender=origin_model, weak=False)
     def model_versioning(**kwargs):
         instance = kwargs['instance']
-        instance_id = instance.id
-        last_version = version_model.objects.filter(origin_id=instance_id).aggregate(last_version=models.Max('version')).get('last_version', 0)
+
+        last_version = version_model.objects.filter(origin=instance).aggregate(last_version=models.Max('version')).get('last_version', 0)
         last_version = last_version if last_version else 0
-        instance.__class__ = version_model
-        instance.id = None
-        instance.origin_id = instance_id
-        instance.version = last_version + 1
-        instance.save()
+
+        new_version_instance = version_model()
+        new_version_instance.origin = instance
+        new_version_instance.version = last_version + 1
+
+        for field in instance._meta.fields:
+            if field.name not in ('id', 'pk'):
+                setattr(new_version_instance, field.name, getattr(instance, field.name))
+
+        new_version_instance.save()
