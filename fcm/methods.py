c77755c91d88839559668397c809decd049fccc2
Suliman Awad ^_^ <sulimanawadstudy@gmail.com>
2024-11-07 10:53:09 +0300

    upgrade fcm app

--- fcm/methods.py
+++ fcm/methods.py
@@ -4,13 +4,24 @@ from enums.enums import FCMType
 from .models import CustomFCMDevice
 
 
+FCM_TYPE_NOTIFICATION = 1
+FCM_TYPE_MESSAGE = 2
+
 FCM_TYPE = 'FCM_TYPE'
+
+
+def clean_data(data: dict):
+    return {
+        key: str(value) for key, value in data.items()
+        if value
+    }
 
 
 def _push_for_users(users, role, data, notification=None):
     if not settings.ACTIVATE_FIREBASE:
         return
-    CustomFCMDevice.objects.filter(user__in=users, role=role).send_message(
+    data = clean_data(data)
+    return CustomFCMDevice.objects.filter(user__in=users, role=role).send_message(
         message=messaging.Message(
             data=data,
             notification=notification,
@@ -28,7 +39,7 @@ def _push_notifications_for_users(
     ) -> None:
     data = { FCM_TYPE: FCMType.NOTIFICATION, **data }
     notification = messaging.Notification(title=title, body=body, image=image)
-    _push_for_users(users=users, role=role, data=data, notification=notification)
+    return _push_for_users(users=users, role=role, data=data, notification=notification)
 
 
 def _push_messages_for_users(
@@ -37,4 +48,4 @@ def _push_messages_for_users(
         data: dict,
     ) -> None:
     data = { FCM_TYPE: FCMType.MESSAGE, **data }
-    _push_for_users(users=users, role=role, data=data, notification=None)
+    return _push_for_users(users=users, role=role, data=data, notification=None)
--- fcm/models.py
+++ fcm/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from safedelete.models import (
+    SafeDeleteManager,
+    SafeDeleteAllManager,
+    SafeDeleteDeletedManager,
+)
 from django.utils.translation import gettext_lazy as _
-from fcm_django.models import AbstractFCMDevice
+from fcm_django.models import AbstractFCMDevice, FCMDeviceQuerySet
 from common.audit.models import HistoricalAuditModel
 from enums.enums import Role
 
@@ -12,3 +17,7 @@ User = get_user_model()
 class CustomFCMDevice(HistoricalAuditModel, AbstractFCMDevice):
     registration_id = models.CharField(max_length=500, verbose_name=_("Registration token"), unique=True)
     role = models.IntegerField(choices=Role.choices)
+
+    objects = SafeDeleteManager(FCMDeviceQuerySet)
+    all_objects = SafeDeleteAllManager(FCMDeviceQuerySet)
+    deleted_objects = SafeDeleteDeletedManager(FCMDeviceQuerySet)
